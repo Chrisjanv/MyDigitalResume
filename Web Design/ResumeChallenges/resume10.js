ec52d1005133dd19c8a19f5c515f14a28c7d184c
Andre Visser <chrisjanv@gmail.com>
2022-11-13 18:06:50 +0200

    resume10

---
+++ Web Design/ResumeChallenges/resume10.js
@@ -0,0 +1,13 @@
+var http=require("http"); 
+var server = http.createServer(function(request, response) {  
+    response.writeHead(200, {  
+        'Content-Type': 'text/plain'  
+    });  
+}); 
+var server = http.createServer(function(request, response) {  
+    response.writeHead(200, {  
+        'Content-Type': 'text/plain'  
+    });  
+});   
+
+document.getElementById("demo").innerHTML=5+6;

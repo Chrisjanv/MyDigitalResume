d8e216b27620120d189ddb381cb21b1ee2147477
Andre Visser <chrisjanv@gmail.com>
2022-11-13 18:45:55 +0200

    no js

--- Web Design/ResumeChallenges/resume10.js
+++ Web Design/ResumeChallenges/resume10.js
@@ -1,8 +1,1 @@
-;document.getElementById("demo").innerHTML = 5 + 6;
-document.getElementById("demo").style.color = "red";
 
-const price1 = 5;
-const price2 = 6;
-let total = price1 + price2;
-
-document.getElementById("total").innerHTML = total
